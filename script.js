6b96435d338aa268d57736677e0b5867aef0b3f4
studickie <dickie.stuart@gmail.com>
2019-02-07 10:19:26 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,113 @@
+// keycodes -> up: 38; down: 40; left: 37; right: 39
+
+let gameGrid = 
+    [
+        ["/","/","/","/","/","/","/","/","/"],
+        ["/","-","-","-","-","-","-","-","/"],
+        ["/","-","-","-","-","-","-","-","/"],
+        ["/","-","-","-","-","-","-","-","/"],
+        ["/","-","-","-","player","-","-","-","/"],
+        ["/","-","-","-","-","-","-","-","/"],
+        ["/","-","-","-","-","-","-","-","/"],
+        ["/","-","-","-","-","-","-","-","/"],
+        ["/","/","/","/","/","/","/","/","/"],
+    ];
+
+$(document).keydown(function(event){
+
+    // pass event to directionSelection() whcih determines movement direction
+    directionSelection(event.which);
+    // after player position value has been modified, render gameGrid to DOM
+    renderGameBoard(gameGrid);
+});
+
+const directionSelection = function(direction) {
+
+    //  pass the direction value to movePlayer() according to keypress
+    if(direction === 38){
+        movePlayer("up");
+
+    } else if (direction === 40){
+        movePlayer("down");
+
+    } else if (direction === 37){
+        movePlayer("left");
+            
+    } else if (direction === 39){
+        movePlayer("right");
+    };
+}
+
+// locate the player value in gameGrid
+const findPlayerPosition = function() {
+
+    const indexY = gameGrid.findIndex(function(spaceY){
+        return spaceY.includes("player");
+    });
+
+    const indexX = gameGrid[indexY].findIndex(function(spaceX){
+        return spaceX.includes("player");
+    });
+
+    // return player position to caller in the form of co-ordinates
+    return [indexY, indexX];
+};
+
+
+const movePlayer = function(direction) {
+
+    const index = findPlayerPosition();
+
+    const indexY = index[0];
+    const indexX = index[1];
+
+    if (direction === "up" && gameGrid[indexY - 1][indexX] != "/"){
+        gameGrid[indexY].splice(indexX, 1, "-");
+        gameGrid[indexY - 1].splice(indexX, 1, "player");
+
+    } else if (direction === "down" && gameGrid[indexY + 1][indexX] != "/"){
+        gameGrid[indexY].splice(indexX, 1, "-");
+        gameGrid[indexY + 1].splice(indexX, 1, "player");
+
+    } else if (direction === "left" && gameGrid[indexY][indexX - 1] != "/"){
+        gameGrid[indexY].splice(indexX, 1, "-");
+        gameGrid[indexY].splice(indexX - 1, 1, "player");
+
+    } else if (direction === "right" && gameGrid[indexY][indexX + 1] != "/"){
+        gameGrid[indexY].splice(indexX, 1, "-");
+        gameGrid[indexY].splice(indexX + 1, 1, "player");
+    };
+};
+
+// render gameGrid to DOM
+const renderGameBoard = function(array) {
+
+    // empty DOM, otherwise it will contain 1000's of layered elements
+    $(".game-board").empty();
+
+    // loop through gameGrid, create and append element based on content
+    array.forEach(function(spaceY, indexY){
+         spaceY.forEach(function(spaceX, indexX){
+            
+            if(spaceX === "/"){
+                $("<div/>").addClass("game-board-border")
+                .css(`left`, `${5 * indexX}rem`)
+                .css(`top`, `${5 * indexY}rem`)
+                .appendTo(".gameBoard")
+
+            } else if (spaceX === "-"){
+                $("<div/>").addClass("game-board-block")
+                .css(`left`, `${5 * indexX}rem`)
+                .css(`top`, `${5 * indexY}rem`)
+                .appendTo(".gameBoard")
+
+            } else if (spaceX === "player"){
+                $("<div/>").addClass("player")
+                .css(`left`, `${5 * indexX}rem`)
+                .css(`top`, `${5 * indexY}rem`)
+                .appendTo(".game-board")
+            }
+        })
+    });
+
+}
